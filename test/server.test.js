98b752d111b1ee35c612c1cbeb9fdc83b21ce597
Rômulo Nunes <romulotg21@gmail.com>
2023-09-27 15:37:37 -0300

    delete: server test

--- test/server.test.js
+++
@@ -1,8 +0,0 @@
-const supertest = require("supertest");
-const request = supertest("http://localhost:3009");
-
-test("Deve responder na porta 3009", () => {
-  // Acessar a url http://localhost:3009
-  // Verificar se a respota é 200. (OK)
-  return request.get("/").then((res) => expect(res.status).toBe(200));
-});
