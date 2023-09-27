9289762a0791a868e169fc36c514f09fd6f767a3
Rômulo Nunes <romulotg21@gmail.com>
2023-09-27 13:48:11 -0300

    chore: ESLINT

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: "airbnb-base",
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.js", ".eslintrc.cjs"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "eol-last": ["error", "always"],
+    quotes: ["error", "double"],
+    "no-console": "off",
+    "linebreak-style": ["error", "windows"],
+  },
+};
